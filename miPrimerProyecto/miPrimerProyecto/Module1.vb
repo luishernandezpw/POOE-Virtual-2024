ccb67fbddc3c0f1b3d1272156b4f456047c367eb
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-17 20:18:45 -0600

    Ejercicio de suma de dos numeros

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,8 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Bienvenidos Chicos de Programacion Orientada a Objetos y Eventos")
+        Console.Write("Num 1: ")
+        Dim num1 As Double
+        num1 = Console.ReadLine()
 
+        Console.Write("Num 2: ")
+        Dim num2 As Double
+        num2 = Console.ReadLine()
+
+        Dim suma As Double
+        suma = num1 + num2
+
+        Console.WriteLine("La suma es: {0}", suma)
+
+        Console.ReadKey() 'Pausa.
     End Sub
 
 End Module
