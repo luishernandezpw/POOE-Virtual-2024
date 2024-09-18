ce9265030d4c03c7ee1f3a97c4af8b5d7fa8cfa6
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-17 20:43:12 -0600

    Ejercicio 2: Determinar el mayor de 3 numeros, introducidos por el usuario

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,18 +1,27 @@
 ﻿Module Module1
 
     Sub Main()
+        'Ejercicio 2: Determinar el mayor de 3 números.
+        Dim num1, num2, num3, mayor As Integer
+
         Console.Write("Num 1: ")
-        Dim num1 As Double
-        num1 = Double.Parse(Console.ReadLine())
+        num1 = Console.ReadLine()
 
         Console.Write("Num 2: ")
-        Dim num2 As Double
-        num2 = Double.Parse(Console.ReadLine())
+        num2 = Console.ReadLine()
 
-        Dim suma As Double
-        suma = num1 + num2
+        Console.Write("Num 3: ")
+        num3 = Console.ReadLine()
 
-        Console.WriteLine("La suma es: {0}", suma)
+        If num1 > num2 Then
+            mayor = num1
+        Else
+            mayor = num2
+        End If
+        If num3 > mayor Then
+            mayor = num3
+        End If
+        Console.WriteLine("EL mayor de los tres numeros es: {0}", mayor)
 
         Console.ReadKey() 'Pausa.
     End Sub
