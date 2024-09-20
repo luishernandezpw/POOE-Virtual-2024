ca34fef910343db43787ec6cd748953544249693
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-19 19:18:55 -0600

    Ejercicio 3. Determinarsi un numero si numero es par o impar.

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,29 +1,23 @@
 ﻿Module Module1
 
     Sub Main()
-        'Ejercicio 2: Determinar el mayor de 3 números.
-        Dim num1, num2, num3, mayor As Integer
+        'Ejercicio 3. Determinarsi un numero si numero es par o impar.
+        Dim continuar As String = "s"
+        While continuar = "s"
+            Dim num As Integer
 
-        Console.Write("Num 1: ")
-        num1 = Console.ReadLine()
+            Console.Write("Num: ")
+            num = Console.ReadLine()
 
-        Console.Write("Num 2: ")
-        num2 = Console.ReadLine()
+            Dim respuesta As String = "Impar"
+            If num Mod 2 = 0 Then
+                respuesta = "Par"
+            End If
+            Console.WriteLine("El numero {0} es: {1}", num, respuesta)
 
-        Console.Write("Num 3: ")
-        num3 = Console.ReadLine()
-
-        If num1 > num2 Then
-            mayor = num1
-        Else
-            mayor = num2
-        End If
-        If num3 > mayor Then
-            mayor = num3
-        End If
-        Console.WriteLine("EL mayor de los tres numeros es: {0}", mayor)
-
-        Console.ReadKey() 'Pausa.
+            Console.Write("Desea continuar? Presiones S, sino cualquier otra tecla: ")
+            continuar = Console.ReadLine()
+        End While
     End Sub
 
 End Module
