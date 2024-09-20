474b6125ba99b433e4a8e7fa09d5dee5e9bc09ad
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-19 20:04:32 -0600

    Resolucion de ejercicios, par impar, primos, fibonacci

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,26 +1,75 @@
 ﻿Module Module1
 
     Sub Main()
-        'Ejercicio 3. Determinarsi un numero si numero es par o impar.
         Dim continuar As String = "s"
         While continuar = "s"
-            Dim num As Integer
+            Console.WriteLine("::.. MENU ..::")
+            Console.WriteLine("1. Determinar si un numero es par o impar.")
+            Console.WriteLine("2. Determinar si un numero es primo o no.")
+            Console.WriteLine("3. Serie Fibonacci.")
+            Console.WriteLine("4. Salir.")
+            Console.Write("Opcion: ")
+            Dim opcion As Integer = Console.ReadLine()
+            Console.Clear()
+            Select Case opcion
+                Case 1
+                    parImpar()
+                Case 2
+                    primo()
+                Case 3
+                    fibonacci()
+                Case Else
+                    continuar = "n"
+            End Select
 
-            Console.Write("Num: ")
-            num = Console.ReadLine()
-
-            Dim respuesta As String = "Impar"
-            If (determinarParImpar(num)) Then
-                respuesta = "Par"
-            End If
-            Console.WriteLine("El numero {0} es: {1}", num, respuesta)
-
-            Console.Write("Desea continuar? Presiones S, sino cualquier otra tecla: ")
-            continuar = Console.ReadLine()
         End While
     End Sub
-    Function determinarParImpar(ByVal num As Integer)
-        Return num Mod 2 = 0
+    Sub fibonacci()
+        Console.Write("Maximo a mostrar: ")
+        Dim n As Integer = Integer.Parse(Console.ReadLine())
+        Dim a = 1, b = 0, c As Integer = 1
+
+        While c <= n
+            c = a + b
+            Console.WriteLine("{0}+{1}={2}", a, b, c)
+            b = a
+            a = c
+        End While
+    End Sub
+    Sub primo()
+        'Un numero es primo solo si es divisible entre el mismo y la unidad.
+        Dim respuesta As String = "NO es primo"
+        Dim num, n, i As Integer
+        n = 0
+        i = 1
+        Console.Write("Num: ")
+        num = Console.ReadLine()
+
+        While (n <= 2 And i <= num)
+            If (determinarParImpar(num, i)) Then
+                n = n + 1
+            End If
+            i = i + 1
+        End While
+        If (n <= 2) Then
+            respuesta = "SI es primo"
+        End If
+        Console.WriteLine("El numero {0} {1}", num, respuesta)
+    End Sub
+    Sub parImpar()
+        Dim num As Integer
+
+        Console.Write("Num: ")
+        num = Console.ReadLine()
+
+        Dim respuesta As String = "Impar"
+        If (determinarParImpar(num, 2)) Then
+            respuesta = "Par"
+        End If
+        Console.WriteLine("El numero {0} es: {1}", num, respuesta)
+    End Sub
+    Function determinarParImpar(ByVal num As Integer, ByVal i As Integer)
+        Return num Mod i = 0
     End Function
 
 End Module
