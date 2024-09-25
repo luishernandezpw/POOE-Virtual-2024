68cd6f395881e0b1c1367ce4e2ea358f428b3fea
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-24 19:40:03 -0600

    Creacion de la clase Estadistica y los objetos

---
+++ miPrimerProyecto/miPrimerProyecto/Estadistica.vb
@@ -0,0 +1,11 @@
+﻿Public Class Estadistica
+    Dim suma As Double
+
+    Public Function armonica(ByVal serie As String)
+        Dim valores() As String = serie.Split(",")
+        For Each valor As String In valores
+            suma += 1 / valor
+        Next
+        Return valores.Length / suma
+    End Function
+End Class
--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,100 +1,14 @@
 ﻿Module Module1
+    Dim objEstadistica As New Estadistica
 
     Sub Main()
-        Dim continuar As String = "s"
-        While continuar = "s"
-            Console.WriteLine("::.. MENU ..::")
-            Console.WriteLine("1. Determinar si un numero es par o impar.")
-            Console.WriteLine("2. Determinar si un numero es primo o no.")
-            Console.WriteLine("3. Serie Fibonacci.")
-            Console.WriteLine("4. Tabla Multiplicar.")
-            Console.WriteLine("5. Media Armonica.")
-            Console.WriteLine("6. Salir.")
-            Console.Write("Opcion: ")
-            Dim opcion As Integer = Console.ReadLine()
-            Console.Clear()
-            Select Case opcion
-                Case 1
-                    parImpar()
-                Case 2
-                    primo()
-                Case 3
-                    fibonacci()
-                Case 4
-                    tablaMultiplicar()
-                Case 5
-                    mediaArmonica()
-                Case Else
-                    continuar = "n"
-            End Select
-        End While
-    End Sub
-    Sub mediaArmonica()
-        Dim serie() As String
-        Dim suma As Double = 0
-        Dim armonica As Double = 0
         Console.Write("Serie: ")
-        serie = Console.ReadLine().Split(",")
-        For Each num As Integer In serie
-            suma = suma + 1 / num
-        Next
-        armonica = serie.Length / suma
-        Console.WriteLine("La media armonica es: {0}", Math.Round(armonica, 1))
-    End Sub
-    Sub tablaMultiplicar()
-        Console.Write("Tabla: ")
-        Dim i = 0, ntabla As Integer = Console.ReadLine()
+        Dim serie As String = Console.ReadLine()
+        Dim armonica As Double = objEstadistica.armonica(serie)
 
-        For i = 1 To 10 Step 1
-            Console.WriteLine("{0}x{1}={2}", ntabla, i, ntabla * i)
-        Next
-    End Sub
-    Sub fibonacci()
-        Console.Write("Maximo a mostrar: ")
-        Dim n As Integer = Console.ReadLine()
-        Dim a = 1, b = 0, c As Integer = 1
+        Console.WriteLine("Media armonica: {0}", Math.Round(armonica, 2))
 
-        While c <= n
-            c = a + b
-            Console.WriteLine("{0}+{1}={2}", a, b, c)
-            b = a
-            a = c
-        End While
+        Console.ReadKey() 'Pausa
     End Sub
-    Sub primo()
-        'Un numero es primo solo si es divisible entre el mismo y la unidad.
-        Dim respuesta As String = "NO es primo"
-        Dim num, n, i As Integer
-        n = 0
-        i = 1
-        Console.Write("Num: ")
-        num = Console.ReadLine()
-
-        While (n <= 2 And i <= num)
-            If (determinarParImpar(num, i)) Then
-                n = n + 1
-            End If
-            i = i + 1
-        End While
-        If (n <= 2) Then
-            respuesta = "SI es primo"
-        End If
-        Console.WriteLine("El numero {0} {1}", num, respuesta)
-    End Sub
-    Sub parImpar()
-        Dim num As Integer
-
-        Console.Write("Num: ")
-        num = Console.ReadLine()
-
-        Dim respuesta As String = "Impar"
-        If (determinarParImpar(num, 2)) Then
-            respuesta = "Par"
-        End If
-        Console.WriteLine("El numero {0} es: {1}", num, respuesta)
-    End Sub
-    Function determinarParImpar(ByVal num As Integer, ByVal i As Integer)
-        Return num Mod i = 0
-    End Function
 
 End Module
