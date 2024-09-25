8e6baa09d87ca91c57da62b4e036c64c083eff70
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-24 20:24:33 -0600

    Saludo

--- poo/poo/Form1.Designer.vb
+++ poo/poo/Form1.Designer.vb
@@ -22,10 +22,57 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnProcesar
+        '
+        Me.btnProcesar.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnProcesar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnProcesar.Location = New System.Drawing.Point(170, 141)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(162, 55)
+        Me.btnProcesar.TabIndex = 0
+        Me.btnProcesar.Text = "Saludar"
+        Me.btnProcesar.UseVisualStyleBackColor = True
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNombre.ForeColor = System.Drawing.Color.Green
+        Me.lblNombre.Location = New System.Drawing.Point(100, 90)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(90, 22)
+        Me.lblNombre.TabIndex = 1
+        Me.lblNombre.Text = "Nombre:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(196, 94)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(251, 20)
+        Me.txtNombre.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(611, 303)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.btnProcesar)
+        Me.Name = "Form1"
+        Me.Text = "Saludar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents txtNombre As TextBox
 End Class
--- poo/poo/Form1.vb
+++ poo/poo/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        MessageBox.Show("Hola, " + txtNombre.Text, "Saludo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
