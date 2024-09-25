1b5a87a60978dc6544b9a5f89fca946c07d49c2e
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-24 20:39:47 -0600

    Calculadora basica: suma, resta, multiplicacion, y division

--- poo/poo/Form1.Designer.vb
+++ poo/poo/Form1.Designer.vb
@@ -23,47 +23,94 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnProcesar = New System.Windows.Forms.Button()
-        Me.lblNombre = New System.Windows.Forms.Label()
-        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.cboOpcion = New System.Windows.Forms.ComboBox()
+        Me.lblOpcion = New System.Windows.Forms.Label()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblNum2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnProcesar
         '
         Me.btnProcesar.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnProcesar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.btnProcesar.Location = New System.Drawing.Point(170, 141)
+        Me.btnProcesar.Location = New System.Drawing.Point(179, 146)
         Me.btnProcesar.Name = "btnProcesar"
         Me.btnProcesar.Size = New System.Drawing.Size(162, 55)
         Me.btnProcesar.TabIndex = 0
-        Me.btnProcesar.Text = "Saludar"
+        Me.btnProcesar.Text = "Calcular"
         Me.btnProcesar.UseVisualStyleBackColor = True
         '
-        'lblNombre
+        'lblNum1
         '
-        Me.lblNombre.AutoSize = True
-        Me.lblNombre.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblNombre.ForeColor = System.Drawing.Color.Green
-        Me.lblNombre.Location = New System.Drawing.Point(100, 90)
-        Me.lblNombre.Name = "lblNombre"
-        Me.lblNombre.Size = New System.Drawing.Size(90, 22)
-        Me.lblNombre.TabIndex = 1
-        Me.lblNombre.Text = "Nombre:"
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1.ForeColor = System.Drawing.Color.Green
+        Me.lblNum1.Location = New System.Drawing.Point(12, 25)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(75, 22)
+        Me.lblNum1.TabIndex = 1
+        Me.lblNum1.Text = "Num 1:"
         '
-        'txtNombre
+        'txtNum1
         '
-        Me.txtNombre.Location = New System.Drawing.Point(196, 94)
-        Me.txtNombre.Name = "txtNombre"
-        Me.txtNombre.Size = New System.Drawing.Size(251, 20)
-        Me.txtNombre.TabIndex = 2
+        Me.txtNum1.Location = New System.Drawing.Point(93, 29)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(136, 20)
+        Me.txtNum1.TabIndex = 2
+        '
+        'cboOpcion
+        '
+        Me.cboOpcion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboOpcion.FormattingEnabled = True
+        Me.cboOpcion.Items.AddRange(New Object() {"Suma", "Resta", "Multiplicacion", "Division"})
+        Me.cboOpcion.Location = New System.Drawing.Point(338, 62)
+        Me.cboOpcion.Name = "cboOpcion"
+        Me.cboOpcion.Size = New System.Drawing.Size(155, 21)
+        Me.cboOpcion.TabIndex = 3
+        '
+        'lblOpcion
+        '
+        Me.lblOpcion.AutoSize = True
+        Me.lblOpcion.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOpcion.ForeColor = System.Drawing.Color.Green
+        Me.lblOpcion.Location = New System.Drawing.Point(248, 58)
+        Me.lblOpcion.Name = "lblOpcion"
+        Me.lblOpcion.Size = New System.Drawing.Size(84, 22)
+        Me.lblOpcion.TabIndex = 4
+        Me.lblOpcion.Text = "Opcion:"
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(93, 81)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(136, 20)
+        Me.txtNum2.TabIndex = 6
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum2.ForeColor = System.Drawing.Color.Green
+        Me.lblNum2.Location = New System.Drawing.Point(12, 77)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(75, 22)
+        Me.lblNum2.TabIndex = 5
+        Me.lblNum2.Text = "Num 2:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.ClientSize = New System.Drawing.Size(611, 303)
-        Me.Controls.Add(Me.txtNombre)
-        Me.Controls.Add(Me.lblNombre)
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(537, 246)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblOpcion)
+        Me.Controls.Add(Me.cboOpcion)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.lblNum1)
         Me.Controls.Add(Me.btnProcesar)
         Me.Name = "Form1"
         Me.Text = "Saludar"
@@ -73,6 +120,10 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents btnProcesar As Button
-    Friend WithEvents lblNombre As Label
-    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents lblNum1 As Label
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents cboOpcion As ComboBox
+    Friend WithEvents lblOpcion As Label
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents lblNum2 As Label
 End Class
--- poo/poo/Form1.vb
+++ poo/poo/Form1.vb
@@ -1,5 +1,23 @@
 ﻿Public Class Form1
     Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        MessageBox.Show("Hola, " + txtNombre.Text, "Saludo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+        Dim opcion As Integer = cboOpcion.SelectedIndex
+        Dim respuesta As Double
+        Select Case opcion
+            Case 0
+                respuesta = num1 + num2
+            Case 1
+                respuesta = num1 - num2
+            Case 2
+                respuesta = num1 * num2
+            Case 3
+                respuesta = num1 / num2
+        End Select
+        MessageBox.Show("La respuesta es: " & respuesta, "Respuesta", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboOpcion.SelectedIndex = 0
     End Sub
 End Class
